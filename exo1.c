997001ec5833d14f4dafe6ea7e701751e1bdef1d
JulienRevest <72550874+JulienRevest@users.noreply.github.com>
2024-10-14 10:59:19 +0200

    TP 1 et TP 2

---
+++ exo1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//#include <stdlib.h>
+
+int main() {
+    /* Affichage du texte du TP */
+    printf("TP 1 Syntaxe et structure\n\tHello world!\n");
+    return 0;
+}
---
+++ exo2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//#include <stdlib.h>
+int a = 10;
+long b = 150;
+short c = 8;
+float d = 14975.79;
+double e = 146783521.2978354162;
+char f = 'y';
+
+int main() {
+    /* Affichage du titre du TP */
+    printf("TP 2: Memoire et Variables\n");
+    /* Affichage de la valeur de chaque variable et de leurs taille en mémoire */
+    printf("Mon int = %d et fait %lu octets\n", a, sizeof(a));
+    printf("Mon long = %ld et fait %lu octets\n", b, sizeof(b));
+    printf("Mon short = %hd et fait %lu octets\n", c, sizeof(c));
+    printf("Mon float = %f et fait %lu octets\n", d, sizeof(d));
+    printf("Mon double = %f et fait %lu octets\n", e, sizeof(e));
+    printf("Mon char = %c et fait %lu octets\n", f, sizeof(f));
+    return 0;
+}
