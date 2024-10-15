353a36dae97a49a0c1c8ca4551b7f12768438cec
JulienRevest <julien.revest@gmail.com>
2024-10-15 10:58:47 +0200

    Improve TP7

--- TP_7.c
+++ TP_7.c
@@ -16,45 +16,30 @@ int main() {
         /* Génération d'un nombre par l'ordinateur*/
         jeu_ordinateur = (rand() % VALEUR_MAX) + VALEUR_MIN;
         printf("Que voulez-vous jouer (1- Pierre, 2- Papier, 3- Ciseaux): ");
-        scanf("%d", &jeu_utilisateur);
+
         /* Vérification de la validité de l'entrée utilisateur */
-        if(jeu_utilisateur < VALEUR_MIN || jeu_utilisateur > VALEUR_MAX) {
+        if(scanf("%d", &jeu_utilisateur) != 1 || !(jeu_utilisateur >= VALEUR_MIN && jeu_utilisateur <= VALEUR_MAX)) {
+            printf("%d", jeu_utilisateur);
             printf("Veuillez rentrer une valeur entre %d et %d\n", VALEUR_MIN, VALEUR_MAX);
+            /* Nettoyage du input buffer */
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF) { }
             continue;
         }
+
         printf("Joueur joue %d et ordinateur joue %d >>>> ", jeu_utilisateur, jeu_ordinateur);
         /* Match null, sinon on vérifie les autres possibilités */
         if(jeu_ordinateur == jeu_utilisateur) {
             printf("match null\n");
         } else {
-            switch (jeu_utilisateur) {
-                case 1: // Pierre
-                    if (jeu_ordinateur == 2) {
-                        printf("Ordinateur gagne 1 point\n");
-                        pt_ordi++;
-                    } else {
-                        printf("Joueur gagne 1 point\n");
-                        pt_joueur++;
-                    }
-                    break;
-                case 2: // Feuille
-                    if (jeu_ordinateur == 3) {
-                        printf("Ordinateur gagne 1 point\n");
-                        pt_ordi++;
-                    } else {
-                        printf("Joueur gagne 1 point\n");
-                        pt_joueur++;
-                    }
-                    break;
-                case 3: // Cisceaux
-                    if (jeu_ordinateur == 1) {
-                        printf("Ordinateur gagne 1 point\n");
-                        pt_ordi++;
-                    } else {
-                        printf("Joueur gagne 1 point\n");
-                        pt_joueur++;
-                    }
-                    break;
+            if((jeu_utilisateur == 1 && jeu_ordinateur == 2) ||  // Pierre vs. feuille
+               (jeu_utilisateur == 2 && jeu_ordinateur == 3) ||  // Feuille vs. cisceaux
+               (jeu_utilisateur == 3 && jeu_ordinateur == 1) ) { // Cisceaux vs. pierre
+                printf("Ordinateur gagne 1 point\n");
+                pt_ordi++;
+            } else {
+                printf("Joueur gagne 1 point\n");
+                pt_joueur++;
             }
         }
         /* Affichage des points*/
